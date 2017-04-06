fa4e7cc150e62194d9240e6fc887613f524003f4
akbarhakim29 <akhapr777@gmail.com>
2017-04-06 18:30:40 +0700

    +add adapter and fragment

---
+++ fragmentrecycleview/src/main/java/com/bymankind/fragmentrecycleview/BlankFragment.java
@@ -0,0 +1,39 @@
+package com.bymankind.fragmentrecycleview;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Server-Panduit on 4/6/2017.
+ */
+
+public class BlankFragment extends Fragment {
+    public BlankFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_blank,container,false);
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view);
+        rv.setHasFixedSize(true);
+        MyAdapter adapter = new MyAdapter(new String[]{"Example one","Example two","Example three","Example four","Example five","Example six","Example seven"});
+        rv.setAdapter(adapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+
+        return rootView;
+    }
+}
